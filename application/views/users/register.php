70c30c61cc15d0474090838078d4ea2868e72ef1
David <pazd6555@gmail.com>
2020-06-18 17:14:18 -0500

    ID registro
    cambio en valor de id

--- application/views/users/register.php
+++ application/views/users/register.php
@@ -1,7 +1,7 @@
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('users/register'); ?>
-	<div class="row">
+	<div class="row" id="1" method>
 		<div class="col-md-4 col-md-offset-4">
 			<h1 class="text-center"><?= $title; ?></h1>
 			<div class="form-group">
